b7ba6ceca3fdfae3326d0e45fa50ddd1c3aae440
Aldedale32 <45789910+Aldedale32@users.noreply.github.com>
2020-12-16 19:11:20 +0100

    Application Dash

--- app.py
+++ app.py
@@ -14,6 +14,19 @@ import pandas as pd
 #Création des données à partir du relevé météo
 df = pd.read_csv("./donnees-synop-essentielles-omm.csv", sep=';')
 
+#Création de la matrice de corrélation
+fig = px.scatter_matrix(df,
+         dimensions=["Temperature", "Pnt_rosee", "Humidite", "Hteur_base_nuages"],
+         title="Matrice de Corrélation : "
+    )
+fig.update_traces(diagonal_visible=False)
+
+fig2 = px.scatter(
+        df,
+        x="Temperature", y="Pnt_rosee",
+        title="Point de rosée en fonction de la température"
+    )
+
 #Création de l'application
 external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
@@ -42,39 +55,19 @@ app.layout = html.Div(className="", children=[
         html.H3("Quelques graphiques : "),
         #Premier graphique
         html.Div("On regarde tout d'abord les corrélations entre les variables"), 
-        html.Div("(Indice : Bonne corrélation Temperature/Point de Rosée et Temperature/Humidité)"), 
-        # On veut une matrice de corrélation entre 4 variables, donc 4 menus déroulants
         
-        dcc.Dropdown(
-            id="x1-dropdown",
-            value="Temperature",
-            options=[{"label": name, "value": name} for name in df.columns],
-        ),
-        dcc.Dropdown(
-            id="x2-dropdown",  
-            value="Pnt_rosee",  
-            options=[{"label": name, "value": name} for name in df.columns],
-        ),
-        dcc.Dropdown(
-            id="x3-dropdown",
-            value="Humidite",
-            options=[{"label": name, "value": name} for name in df.columns],
-        ),
-        dcc.Dropdown(
-            id="x4-dropdown",  
-            value="Hteur_base_nuages",  
-            options=[{"label": name, "value": name} for name in df.columns],
-        ),
         #Emplacement de la matrice
         html.Div(
-            dcc.Graph(id='graph'),
+            dcc.Graph(figure=fig),
         ),
         
         #On saute une ligne
         html.Hr(),
         
         #Deuxième graphique
-        html.Div("Regardons de plus près le couple Temperature/Point de rosée: "),
+        html.Div("Sur la matrice seule 4 variables sont disponibles, on peut aussi regarder librement la corrélation entre deux variables quelconques"), 
+        html.Div("(Indice : Bonne corrélation Temperature/Point de Rosée et Temperature/Humidité)"), 
+
         # Ici on veut un graphique simple à deux dimensions, donc deux menus déroulants
         
         dcc.Dropdown(
@@ -85,17 +78,25 @@ app.layout = html.Div(className="", children=[
 
         dcc.Dropdown(
             id="y-dropdown",
-            value="Pnt_rosee",
+            value="Hteur_base_nuages",
             options=[{"label": name, "value": name} for name in df.columns],
         ),
         
         # Emplacement du graphique
         html.Div(
-            dcc.Graph(id='graph2'),
+            dcc.Graph(id='graph'),
+        ),
+        
+        #Troisième graphique
+        html.Div("Regardons de plus près le couple Temperature/Point de rosée: "),
+        
+        #Emplacement du graphique
+        html.Div(
+            dcc.Graph(figure=fig2),
         ),
         
         #Quelques explications
-        html.Div("Merveilleux, on voit que la température et la température sous laquelle la rosée se dépose naturellement sont corrélées, il va falloir expliquer le phénomène : \n    -on obtient un graphe similaire aux approximations d'August-Roche-Magnus; \n  -le point de rosée correspond aussi à la température à laquelle la pression partielle de vapeur d'eau est égale à sa pression de vapeur saturante; \n  -On peut donc calculer l'humidité grâce au point de rosée.")
+        html.Div("Merveilleux, on voit que la température et la température sous laquelle la rosée se dépose naturellement sont corrélées, il va falloir expliquer le phénomène : On obtient un graphe similaire aux approximations d'August-Roche-Magnus; Le point de rosée correspond aussi à la température à laquelle la pression partielle de vapeur d'eau est égale à sa pression de vapeur saturante. On peut donc calculer l'humidité grâce au point de rosée.")
         
     ]),
 
@@ -117,36 +118,15 @@ app.layout = html.Div(className="", children=[
 
 @app.callback(
     Output('graph', 'figure'),
-    #On retrouve les 4 valeurs des menus déroulants correspondant aux 4 variables à entrer dans la matrice
-    [Input("x1-dropdown", "value"), 
-     Input("x2-dropdown", "value"),
-     Input("x3-dropdown", "value"),
-     Input("x4-dropdown", "value")],
-)
-def display_graph(x1value, x2value, x3value, x4vaue):
-    #On crée la matrice
-    figure = px.scatter_matrix(df,
-         dimensions=[x1value, x2value, x3value, x4value],
-         title="Matrice de Corrélation : "
-    )
-    figure.update_traces(diagonal_visible=False)
-                              
-    #On retourne la matrice
-    return figure
-
-
-@app.callback(
-    Output('graph2', 'figure'),
     #On retrouve les deux valeurs des menus déroulants correspondant aux deux dimensions du graphique
     [Input("x-dropdown", "value"),
      Input("y-dropdown", "value")],
 )
-def display_graph2(xvalue, yvalue):
+def display_graph(xvalue, yvalue):
     #On crée le graphique
     figure = px.scatter(
         df,
         x=xvalue, y=yvalue,
-        title="Point de rosée en fonction de la température"
     )
     #On retourne le graphique
     return figure
